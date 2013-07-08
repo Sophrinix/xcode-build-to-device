cd1b555dff00c90d290d4a2f18c1afc08c9587dd
Jason Kneen <jason.kneen@bouncingfish.com>
2013-07-08 09:04:39 +0100

    Tweaked to support path with spaces e.g. "Titanium Studio Workspace"

--- build2phone.sh
+++ build2phone.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
- xcodebuild -project `pwd`/build/iphone/*.xcodeproj -configuration Debug  -sdk iphoneos CONFIGURATION_BUILD_DIR="`pwd`/build/iphone/build/"
+ xcodebuild -project "`pwd`"/build/iphone/*.xcodeproj -configuration Debug  -sdk iphoneos CONFIGURATION_BUILD_DIR="`pwd`/build/iphone/build/"
 
 
-~/.b2i/bin/mobiledevice install_app `pwd`/build/iphone/build/*.app
+~/.b2i/bin/mobiledevice install_app "`pwd`"/build/iphone/build/*.app
 
   
 
