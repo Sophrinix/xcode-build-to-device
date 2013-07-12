b2e5fc0bb451b7a58b970fc84172f55f05bf5799
Andrew McElroy <sophrinix@gmail.com>
2013-07-12 11:30:52 -0500

    a much better experience. auto cleans every time b2i is ran

--- build2phone.sh
+++ build2phone.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# rebuild the project everytime
+
+titanium clean
+
+titanium build -b --platform ios
+
 
  xcodebuild -project "`pwd`"/build/iphone/*.xcodeproj -configuration Debug  -sdk iphoneos CONFIGURATION_BUILD_DIR="`pwd`/build/iphone/build/"
 
